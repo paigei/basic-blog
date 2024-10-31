7340bcc5cfdf5c0968d9fd6337953e20000357c8
paigei <113061249+paigei@users.noreply.github.com>
2024-10-30 17:32:53 -0700

    making changes to article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,10 +2,13 @@ export default function Article({ article }) {
   return (
     <article>
       {!article ? (
-        <p>No article selected</p>
+        <div>
+          <h2>Welcome!!</h2>
+          <p>No article selected</p>
+        </div>
       ) : (
         <section>
-          <h2>{article.title}</h2>
+          <h2 className="title">{article.title}</h2>
           <p className="date">{`Posted: ${article.date}`}</p>
           <p className="body">{article.body}</p>
         </section>
