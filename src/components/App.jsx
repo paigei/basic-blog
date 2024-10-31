5d2d0c5e51f7932999af029256471e241f02c4ef
paigei <113061249+paigei@users.noreply.github.com>
2024-10-30 17:48:56 -0700

    added button to nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,11 +37,10 @@ export default function App() {
     <div className="App">
       <header>
         Crochet Blog!
-        {user && <button class="new-art" onClick={() => setWriting(true)}>New Article</button>}
         {!user ? <SignIn /> : <SignOut />}
       </header>
 
-      {!user ? "" : <Nav articles={articles} setArticle={setArticle} />}
+      {!user ? "" : <Nav articles={articles} setArticle={setArticle} user={user} setWriting={setWriting}/>}
 
       {!user ? (
         ""
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
-export default function Nav({ articles, setArticle }) {
+export default function Nav({ articles, setArticle, user, setWriting }) {
   return (
     <nav>
+      {user && <button class="new-art" onClick={() => setWriting(true)}>New Article</button>}
       {!articles
         ? "No articles"
         : articles.map((a) => (
